1cba557a5049959e82b53983c2b127354357e188
noobj <jingfu99@gmail.com>
2022-07-15 14:20:20 +0800

    test: add crowdscraper test

--- cmd/dailyfetch/main_test.go
+++ cmd/dailyfetch/main_test.go
@@ -5,16 +5,24 @@ import (
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/noobj/swim-crowd-lambda-go/cmd/dailyfetch/models"
+	container "github.com/golobby/container/v3"
+	"github.com/noobj/swim-crowd-lambda-go/internal/repositories"
+	EntryRepositories "github.com/noobj/swim-crowd-lambda-go/internal/repositories/entry"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MockEntryModel struct{}
 
-func (m MockEntryModel) FetchEntriesByTimeRange(start string, end string) []models.EntryGroup {
-	return []models.EntryGroup{
+func (m MockEntryModel) InsertOne(doc bson.D) {
+
+}
+
+func (m MockEntryModel) Aggregate(stages []bson.D) *mongo.Cursor {
+	fakeData := []EntryRepositories.EntryGroup{
 		{
 			Date: "2022-07-13",
-			Entries: []models.Entry{
+			Entries: []EntryRepositories.Entry{
 				{
 					Amount: 1234,
 					Time:   "2022-07-13 15:00",
@@ -22,12 +30,24 @@ func (m MockEntryModel) FetchEntriesByTimeRange(start string, end string) []mode
 			},
 		},
 	}
+
+	mockData := make([]interface{}, len(fakeData))
+
+	for i := range fakeData {
+		mockData[i] = fakeData[i]
+	}
+
+	cur, _ := mongo.NewCursorFromDocuments(mockData, nil, nil)
+	return cur
 }
 
 func TestHandler(t *testing.T) {
-	app := &Application{entryModel: MockEntryModel{}}
-	res, err := app.Handler(context.TODO(), events.APIGatewayProxyRequest{})
-	expectedRes := "[{\"date\":\"2022-07-13\",\"entries\":[{\"amount\":1234,\"time\":\"2022-07-13 15:00\"}]}]"
+	container.Singleton(func() repositories.Repository {
+		return &MockEntryModel{}
+	})
+	res, err := Handler(context.TODO(), events.APIGatewayProxyRequest{})
+
+	expectedRes := "[{\"date\":\"2022-07-13 (Wed)\",\"entries\":[{\"amount\":1234,\"time\":\"2022-07-13 15:00\"}]}]"
 
 	if expectedRes != res.Body {
 		t.Errorf("\n...expected = %v\n...obtained = %v", expectedRes, res.Body)
